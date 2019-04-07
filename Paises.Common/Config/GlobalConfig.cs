2193bd58c7f9b37ea4b5f6dda74ed1496871178f
Darwin <darwinu34@yahoo.es>
2019-04-07 08:29:53 -0500

    Add project files.

---
+++ Paises.Common/Config/GlobalConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paises.Common.Config
+{
+    class GlobalConfig
+    {
+    }
+}
---
+++ Paises.Infraestructure/Repository/Pais/Paises.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paises.Infraestructure.Repository.Pais
+{
+    public class Paises
+    {
+
+    }
+}
